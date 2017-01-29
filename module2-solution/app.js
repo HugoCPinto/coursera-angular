dd60da401f698f9f75e12aac4e6dc448c4b159f4
Hugo Pinto <hugocpinto@cenas.com>
2017-01-29 16:32:31 +0000

    Full solution module2

---
+++ module2-solution/app.js
@@ -0,0 +1,92 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+// Controller List to buy
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+
+  var toBuyList = this;
+
+  toBuyList.items = ShoppingListCheckOffService.getListToBuy();
+
+  toBuyList.removeItem = function(itemIndex) {
+    ShoppingListCheckOffService.remove(itemIndex);
+  }
+
+  toBuyList.addItemToBoughtList = function(itemName, itemQuantity) {
+    ShoppingListCheckOffService.addItem(itemName, itemQuantity);
+  }
+
+}
+
+// Controller List bought
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+
+  var listBought = this;
+
+  listBought.items = ShoppingListCheckOffService.getListBought();
+
+}
+
+//service
+function ShoppingListCheckOffService() {
+
+  var service = this;
+
+  //list to buy
+  var listToBuy = [
+    {
+      itemName: "Milk",
+      itemQuantity: "2"
+    },
+    {
+      itemName: "Donuts",
+      itemQuantity: "200"
+    },
+    {
+      itemName: "Cookies",
+      itemQuantity: "300"
+    },
+    {
+      itemName: "Chocolate",
+      itemQuantity: "5"
+    }
+  ];
+
+  //list bought
+  var listBought = [];
+
+
+  service.addItem = function (itemName, itemQuantity) {
+
+    var item = {
+      itemName : itemName,
+      itemQuantity : itemQuantity
+    };
+
+    listBought.push(item);
+
+  }
+
+  service.remove = function(itemIndex) {
+    listToBuy.splice(itemIndex, 1);
+  }
+
+  service.getListToBuy = function() {
+    return listToBuy;
+  }
+
+  service.getListBought = function() {
+    return listBought;
+  }
+
+}
+
+})();
