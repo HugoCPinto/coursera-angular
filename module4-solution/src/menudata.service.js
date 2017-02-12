5faf05341f8b712657c7513e6c55c78b77c604ca
Hugo Pinto <hugocpinto@cenas.com>
2017-02-12 12:29:23 +0000

    module 4

---
+++ module4-solution/src/menudata.service.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('data')
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+MenuDataService.$inject = ['$http', 'ApiBasePath']
+function MenuDataService($http, ApiBasePath) {
+  var service = this;
+
+  // get all categories
+  service.getAllCategories = function(){
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    }).then(function (response) {
+      return response.data;
+    });
+  }
+
+  // get menu items for category
+  service.getItemsForCategory = function(categoryShortName){
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      params: {
+        category: categoryShortName
+      }
+    }).then(function (response) {
+      return response.data.menu_items;
+    });
+  }
+}
+
+})();
